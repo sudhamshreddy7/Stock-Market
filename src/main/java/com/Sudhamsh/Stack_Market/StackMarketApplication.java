359ad5a1d2099941000de8b6da6937438864c7a3
sudhamshreddy7 <sudhamshreddy06@gmail.com>
2025-01-14 15:36:15 -0500

    first commit

---
+++ src/main/java/com/Sudhamsh/Stack_Market/StackMarketApplication.java
@@ -0,0 +1,13 @@
+package com.Sudhamsh.Stack_Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sudhamsh/Stack_Market/StackMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sudhamsh.Stack_Market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
